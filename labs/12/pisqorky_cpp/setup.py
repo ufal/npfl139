c65bb776e2ba99d28de440dfc959481284a1a810
Milan Straka <milan@strakovi.com>
2024-05-11 14:22:37 +0200

    Fix incorrect file name.

--- labs/12/pisqorky_cpp/setup.py
+++ labs/12/pisqorky_cpp/setup.py
@@ -6,6 +6,6 @@ setup(
     version="0.0.1",
     ext_modules=[Pybind11Extension(
         "pisqorky_cpp", ["pisqorky_cpp.cpp"], depends=[
-            "pisqorky_cpp.h", "pisqorky_heuristic.h", "pisqorky_mcts.h", "pisqorky_sim_game.h"], cxx_std=17,
+            "pisqorky.h", "pisqorky_heuristic.h", "pisqorky_mcts.h", "pisqorky_sim_game.h"], cxx_std=17,
     )],
 )
