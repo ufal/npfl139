906d3dae2a1b497aafbeb294a1d7b54cb7970c53
Milan Straka <milan@strakovi.com>
2024-05-22 13:37:47 +0200

    Add lecture 14 assignments.
    Templates for memory_game will appear later.

---
+++ labs/14/mappo.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python3
+import argparse
+import os
+
+import gymnasium as gym
+import numpy as np
+import torch
+
+import multi_collect_environment
+import wrappers
+multi_collect_environment.register()
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--agents", default=2, type=int, help="Agents to use.")
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+parser.add_argument("--seed", default=None, type=int, help="Random seed.")
+parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+# For these and any other arguments you add, ReCodEx will keep your default value.
+parser.add_argument("--batch_size", default=..., type=int, help="Batch size.")
+parser.add_argument("--clip_epsilon", default=..., type=float, help="Clipping epsilon.")
+parser.add_argument("--entropy_regularization", default=0.1, type=float, help="Entropy regularization weight.")
+parser.add_argument("--envs", default=..., type=int, help="Workers during experience collection.")
+parser.add_argument("--epochs", default=..., type=int, help="Epochs to train each iteration.")
+parser.add_argument("--evaluate_each", default=10, type=int, help="Evaluate each given number of iterations.")
+parser.add_argument("--evaluate_for", default=10, type=int, help="Evaluate the given number of episodes.")
+parser.add_argument("--gamma", default=..., type=float, help="Discounting factor.")
+parser.add_argument("--hidden_layer_size", default=50, type=int, help="Size of hidden layer.")
+parser.add_argument("--learning_rate", default=3e-4, type=float, help="Learning rate.")
+parser.add_argument("--trace_lambda", default=..., type=float, help="Traces factor lambda.")
+parser.add_argument("--worker_steps", default=..., type=int, help="Steps for each worker to perform.")
+
+
+class Network:
+    # Use GPU if available.
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def __init__(self, observation_space: gym.Space, action_space: gym.Space, args: argparse.Namespace) -> None:
+        self._args = args
+
+        # TODO(ppo): Create an actor using a single hidden layer with `args.hidden_layer_size`
+        # units and ReLU activation, produce a policy with `action_space.n` discrete actions.
+        self._actor = ...
+
+        # TODO(ppo): Create a critic (value predictor) consisting of a single hidden layer with
+        # `args.hidden_layer_size` units and ReLU activation, and and output layer with a single output.
+        self._critic = ...
+
+    def save_actor(self, path: str):
+        torch.save(self._actor.state_dict(), path)
+
+    def load_actor(self, path: str):
+        self._actor.load_state_dict(torch.load(path, map_location=self.device))
+
+    # The `wrappers.typed_torch_function` automatically converts input arguments
+    # to PyTorch tensors of given type, and converts the result to a NumPy array.
+    @wrappers.typed_torch_function(device, torch.float32, torch.int64, torch.float32, torch.float32, torch.float32)
+    def train(self, states: torch.Tensor, actions: torch.Tensor, action_probs: torch.Tensor,
+              advantages: torch.Tensor, returns: torch.Tensor) -> None:
+        # TODO(ppo): Perform a single training step of the PPO algorithm.
+        # For the policy model, the sum is the sum of:
+        # - the PPO loss, where `self._args.clip_epsilon` is used to clip the probability ratio
+        # - the entropy regularization with coefficient `self._args.entropy_regularization`.
+        #   You can compute it for example using the `torch.distributions.Categorical` class.
+        ...
+
+        # TODO(ppo): The critic model is trained in a standard way, by using the MSE
+        # error between the predicted value function and target returns.
+        ...
+
+    @wrappers.typed_torch_function(device, torch.float32)
+    def predict_actions(self, states: torch.Tensor) -> np.ndarray:
+        # TODO(ppo): Return predicted action probabilities.
+        raise NotImplementedError()
+
+    @wrappers.typed_torch_function(device, torch.float32)
+    def predict_values(self, states: torch.Tensor) -> np.ndarray:
+        # TODO(ppo): Return estimates of value function.
+        raise NotImplementedError()
+
+
+def main(env: wrappers.EvaluationEnv, args: argparse.Namespace) -> None:
+    # Set random seeds and the number of threads
+    np.random.seed(args.seed)
+    if args.seed is not None:
+        torch.manual_seed(args.seed)
+    torch.set_num_threads(args.threads)
+    torch.set_num_interop_threads(args.threads)
+
+    # Construct the networks, each for the same observation space and corresponding action space.
+    networks = [Network(env.observation_space, env.action_space[i], args) for i in range(args.agents)]
+
+    def evaluate_episode(start_evaluation: bool = False, logging: bool = True) -> float:
+        rewards, state, done = 0, env.reset(start_evaluation=start_evaluation, logging=logging)[0], False
+        while not done:
+            # TODO: Predict the vector of actions using the greedy policy
+            action = ...
+            state, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+            rewards += reward
+        return rewards
+
+    # Create the vectorized environment
+    venv = gym.make_vec(env.spec.id, args.envs, gym.VectorizeMode.ASYNC, agents=args.agents)
+
+    # Training
+    state = venv.reset(seed=args.seed)[0]
+    training = True
+    iteration = 0
+    while training:
+        # Collect experience. Notably, we collect the following quantities
+        # as tensors with the first two dimensions `[args.worker_steps, args.envs]`,
+        # and the third dimension being `args.agents` for `action*`, `rewards`, `values`.
+        states, actions, action_probs, rewards, dones, values = [], [], [], [], [], []
+        for _ in range(args.worker_steps):
+            # TODO: Choose `action`, which is a vector of `args.agents` actions for each worker,
+            # each action sampled from the corresponding policy generated by the `predict` of the
+            # networks executed on the vector `state`.
+            action = ...
+
+            # Perform the step, extracting the per-agent rewards for training
+            next_state, _, terminated, truncated, info = venv.step(action)
+            reward = np.array([*info["agent_rewards"]])
+            done = terminated | truncated
+
+            # TODO: Collect the required quantities
+            ...
+
+            state = next_state
+
+        for a in range(args.agents):
+            # TODO: For the given agent, estimate `advantages` and `returns` (they differ only by the value
+            # function estimate) using lambda-return with coefficients `args.trace_lambda` and `args.gamma`.
+            # You need to process episodes of individual workers independently, and note that
+            # each worker might have generated multiple episodes, the last one probably unfinished.
+            advantages, returns = ...
+
+            # TODO: Train the agent `a` for `args.epochs` using the collected data. In every epoch,
+            # you should randomly sample batches of size `args.batch_size` from the collected data.
+            # A possible approach is to create a dataset of `(states, actions, action_probs, advantages, returns)`
+            # quintuples using a single `torch.utils.data.StackDataset` and then use a dataloader.
+            ...
+
+        # Periodic evaluation
+        iteration += 1
+        if iteration % args.evaluate_each == 0:
+            returns = [evaluate_episode() for _ in range(args.evaluate_for)]
+
+    # Final evaluation
+    while True:
+        evaluate_episode(start_evaluation=True)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+
+    # Create the environment
+    env = wrappers.EvaluationEnv(gym.make("MultiCollect-v0", agents=args.agents), args.seed, args.render_each)
+
+    main(env, args)
--- lectures/lecture14.md
+++ lectures/lecture14.md
@@ -4,6 +4,9 @@
 #### Reading: https://ufal.mff.cuni.cz/~straka/courses/npfl139/2324/slides.pdf/npfl139-2324-14.pdf,PDF Slides
 #### Video: https://lectures.ms.mff.cuni.cz/video/rec/npfl139/2324/npfl139-2324-14.mp4, Lecture
 #### Questions: #lecture_14_questions
+#### Lecture assignment: mappo
+#### Lecture assignment: memory_game
+#### Lecture assignment: memory_game_rl
 
 - Multi-Agent Reinforcement Learning [Nice overview gives the diploma thesis [Cooperative Multi-Agent Reinforcement Learning](https://dspace.cuni.cz/handle/20.500.11956/127431)]
 - MERLIN model [[Greg Wayne et al.:Unsupervised Predictive Memory in a Goal-Directed Agent](https://arxiv.org/abs/1803.10760)]
