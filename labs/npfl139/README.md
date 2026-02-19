be3f458f00f4c09faa2c2d0dffb1b246df488643
Milan Straka <milan@strakovi.com>
2026-02-19 01:49:29 +0100

    Release npfl139 version 2526.1.0.

---
+++ labs/npfl139/README.md
@@ -0,0 +1,5 @@
+# The `npfl139` Package: Modules Used in the Deep Reinforcement Learning Course (NPFL139)
+
+This package contains the modules used in the
+[Deep Reinforcement Learning course (NPFL139)](http://ufal.mff.cuni.cz/courses/npfl139),
+available under the Mozilla Public License 2.0.
---
+++ labs/npfl139/env_wrappers.py
@@ -0,0 +1,63 @@
+# This file is part of NPFL139 <http://github.com/ufal/npfl139/>.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import gymnasium as gym
+import numpy as np
+
+
+# Discrete environments.
+class DiscretizationWrapper(gym.ObservationWrapper):
+    def __init__(self, env, separators, tiles=None):
+        super().__init__(env)
+        self._separators = separators
+        self._tiles = tiles
+
+        if tiles is None:
+            states = 1
+            for separator in separators:
+                states *= 1 + len(separator)
+            self.observation_space = gym.spaces.Discrete(states)
+        else:
+            self._first_tile_states, self._rest_tiles_states = 1, 1
+            for separator in separators:
+                self._first_tile_states *= 1 + len(separator)
+                self._rest_tiles_states *= 2 + len(separator)
+            self.observation_space = gym.spaces.MultiDiscrete([
+                self._first_tile_states + i * self._rest_tiles_states for i in range(tiles)])
+
+            self._separator_offsets = [0 if len(s) <= 1 else (s[1] - s[0]) / tiles for s in separators]
+            self._separator_tops = [np.inf if len(s) <= 1 else s[-1] + (s[1] - s[0]) for s in separators]
+
+    def observation(self, observations):
+        state = 0
+        for observation, separator in zip(observations, self._separators):
+            state *= 1 + len(separator)
+            state += np.digitize(observation, separator)
+        if self._tiles is None:
+            return state
+        else:
+            states = np.empty(self._tiles, dtype=np.int64)
+            states[0] = state
+            for t in range(1, self._tiles):
+                state = 0
+                for i in range(len(self._separators)):
+                    state *= 2 + len(self._separators[i])
+                    value = observations[i] + ((t * (2 * i + 1)) % self._tiles) * self._separator_offsets[i]
+                    if value > self._separator_tops[i]:
+                        state += 1 + len(self._separators[i])
+                    else:
+                        state += np.digitize(value, self._separators[i])
+                states[t] = self._first_tile_states + (t - 1) * self._rest_tiles_states + state
+            return states
+
+
+class DiscreteCartPoleWrapper(DiscretizationWrapper):
+    def __init__(self, env, bins=8):
+        super().__init__(env, [
+            np.linspace(-2.4, 2.4, num=bins + 1)[1:-1],  # cart position
+            np.linspace(-3, 3, num=bins + 1)[1:-1],      # cart velocity
+            np.linspace(-0.2, 0.2, num=bins + 1)[1:-1],  # pole angle
+            np.linspace(-2, 2, num=bins + 1)[1:-1],      # pole angle velocity
+        ])
