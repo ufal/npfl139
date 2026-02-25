2c3efd1e7a782556ca8b38e6ad67b5fa625ae832
Milan Straka <milan@strakovi.com>
2026-02-25 19:15:35 +0100

    Add lecture 2 assignments.

---
+++ labs/02/q_learning.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+import argparse
+
+import gymnasium as gym
+import numpy as np
+
+import npfl139
+npfl139.require_version("2526.2")
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+parser.add_argument("--seed", default=None, type=int, help="Random seed.")
+# For these and any other arguments you add, ReCodEx will keep your default value.
+parser.add_argument("--alpha", default=..., type=float, help="Learning rate.")
+parser.add_argument("--epsilon", default=..., type=float, help="Exploration factor.")
+parser.add_argument("--gamma", default=..., type=float, help="Discounting factor.")
+
+
+def main(env: npfl139.EvaluationEnv, args: argparse.Namespace) -> None:
+    # Set the random seed.
+    npfl139.startup(args.seed)
+
+    # TODO: Variable creation and initialization
+
+    training = True
+    while training:
+        # Perform episode
+        state, done = env.reset()[0], False
+        while not done:
+            # TODO: Perform an action.
+            action = ...
+
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+
+            # TODO: Update the action-value estimates
+
+            state = next_state
+
+    # Final evaluation
+    while True:
+        state, done = env.reset(start_evaluation=True)[0], False
+        while not done:
+            # TODO: Choose a greedy action
+            action = ...
+            state, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+
+
+if __name__ == "__main__":
+    main_args = parser.parse_args([] if "__file__" not in globals() else None)
+
+    # Create the environment
+    main_env = npfl139.EvaluationEnv(
+        npfl139.DiscreteMountainCarWrapper(gym.make("npfl139/MountainCar1000-v0")),
+        main_args.seed, main_args.render_each,
+    )
+
+    main(main_env, main_args)
